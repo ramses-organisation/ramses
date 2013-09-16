5ed78825e4d74daa0d6b35f405e53c63ce4460fb
Michael Rieder <rieder@physik.uzh.ch>
2013-09-16 11:23:43 +0200

    read in B_ave from namelist file

--- trunk/ramses/mhd/read_hydro_params.f90
+++ trunk/ramses/mhd/read_hydro_params.f90
@@ -40,7 +40,7 @@ subroutine read_hydro_params(nml_ok)
        & ,m_star,t_star,n_star,T2_star,g_star,del_star,eps_star,jeans_ncells &
        & ,eta_sn,yield,rbubble,f_ek,ndebris,f_w,mass_gmc &
        & ,J21,a_spec,z_ave,z_reion,eta_mag,n_sink,bondi,delayed_cooling &
-       & ,self_shielding,smbh,agn,rsink_max,msink_max
+       & ,self_shielding,smbh,agn,rsink_max,msink_max,B_ave
 
   ! Read namelist file
   rewind(1)
