37fda366a4a28cabbf69d558553fac64ac91e3bc
Andreas Bleuler <ableuler@physik.uzh.ch>
2014-08-21 15:24:21 +0200

    keep some changes local for the moment...

--- trunk/ramses/pm/particle_tree.f90
+++ trunk/ramses/pm/particle_tree.f90
@@ -143,7 +143,7 @@ subroutine init_tree
   ! Sort particles down to levelmin
   do ilevel=1,levelmin-1
      call make_tree_fine(ilevel)
-     call virtual_tree_fine(ilevel)
+!     call virtual_tree_fine(ilevel)
      call kill_tree_fine(ilevel)
      ! Update boundary conditions for remaining particles
      call virtual_tree_fine(ilevel)
