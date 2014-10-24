34768e6d30993a1e71973b9b38f5081ae94ee2de
biernacki <biernackip@yahoo.com>
2014-10-24 09:49:12 +0200

    movie routine accepts non-square maps, updated python scripts accordingly

--- trunk/ramses/utils/py/map2img.py
+++ trunk/ramses/utils/py/map2img.py
@@ -23,8 +23,6 @@ def main():
 			help='max value', default=None)
 	parser.add_option("-d","--dir", dest="dir", \
 			help='map directory', default=None)
-	parser.add_option("-i","--iter", dest="iter", \
-			help="iterator index", default=2)
 	parser.add_option("-p","--proj", dest="proj", \
 			help="proj_nd", default=2)
 	parser.add_option("-s","--step", dest="step", \
@@ -88,7 +86,7 @@ def main():
 	pbar = ProgressBar(widgets=widgets, maxval = max_iter+1).start()
 	
 	# Looping over movie snapshots
-	for i in xrange(opts.zero,int(opts.step),max_iter+1,int(opts.step)):
+	for i in xrange(opts.zero+int(opts.step),max_iter+1,int(opts.step)):
 
 		infile = "%smovie%d/%s_%05d.map" % (opts.dir, int(opts.proj), opts.kind, i)		
 	
@@ -174,7 +172,7 @@ def main():
 		dat = dat.reshape(ny,nx)
 	
 		# Plotting
-		fig = plt.figure(figsize=(8,8),frameon=False)
+		fig = plt.figure(figsize=(8,8*ny/nx),frameon=False)
 		
 		ax = fig.add_subplot(1,1,1)
 		ax.set_axis_off()
@@ -198,8 +196,8 @@ def main():
 					marker='+',c='r',mew=1,ms=6)
 
 		plt.axis('off') # removes axis
-		plt.xlim(0,ny) # trims image to borders
-		plt.ylim(0,nx)
+		plt.xlim(0,nx) # trims image to borders
+		plt.ylim(0,ny)
 		# corrects window extent
 		extent = ax.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
 		plt.savefig(outfile,bbox_inches=extent,dpi=100)
@@ -208,7 +206,6 @@ def main():
 		pbar.update(i) # updates progressbar
 
 	pbar.finish()
-		
 
 if __name__ == '__main__':
 	main()
