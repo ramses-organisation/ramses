11bc9b1984e0e0c7d4a8239d319e0d076f16fc08
biernacki <biernackip@yahoo.com>
2014-10-31 14:51:38 +0100

    Movie routine updated. Now user can choose variables saved as snapshots

--- trunk/ramses/amr/read_params.f90
+++ trunk/ramses/amr/read_params.f90
@@ -35,7 +35,7 @@ subroutine read_params
   namelist/movie_params/levelmax_frame,nw_frame,nh_frame,ivar_frame &
        & ,xcentre_frame,ycentre_frame,zcentre_frame &
        & ,deltax_frame,deltay_frame,deltaz_frame,movie &
-       & ,imovout,imov,tendmov,aendmov,proj_axis
+       & ,imovout,imov,tendmov,aendmov,proj_axis,movie_vars
 
   ! MPI initialization
 #ifndef WITHOUTMPI
