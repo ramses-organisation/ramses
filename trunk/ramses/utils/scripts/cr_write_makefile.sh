43e6633c507dfddc774d1310792cc4ab87945aed
Romain Teyssier <teyssier@f1.zbox.physik.uzh.ch>
2014-12-04 08:07:17 +0100

    Modify scripts to make fortran file for makefile and patch outputs.

--- trunk/ramses/utils/scripts/cr_write_makefile.sh
+++ trunk/ramses/utils/scripts/cr_write_makefile.sh
@@ -13,28 +13,24 @@ fi
 
 MAKEFILE=$1
 
-sed 's/\$/ /g' ${MAKEFILE} | sed "s/\"/'/g" | cat -e | sed 's/\$/\"\/\/new_line("A") \&/' | sed 's/^/       \& \/\/\"/' > .test_middle.f90
+sed 's/\$/ /g' ${MAKEFILE} | sed "s/\"/'/g" | cat -e | sed 's/\$/\"/' | sed 's/^/  write(ilun,*)\"/' > .test_middle.f90
   
 cat << EOF > .test_after.f90
-       & //" "
 
-  ilun=myid+10
-
-  fileloc=TRIM(filename)
-  open(unit=ilun,file=fileloc,form='formatted')
-  write(ilun,*)TRIM(content)
   close(ilun)
 end subroutine output_makefile
 EOF
 
 cat << EOF > .test_before.f90
 subroutine output_makefile(filename)
-  character(len=500000)::content
   character(LEN=80)::filename
   character(LEN=80)::fileloc
   integer::ilun
 
-  content=" "//new_line("A") &
+  ilun=myid+10
+
+  fileloc=TRIM(filename)
+  open(unit=ilun,file=fileloc,form='formatted')
 EOF
 
 cat .test_before.f90 .test_middle.f90 .test_after.f90 > write_makefile.f90
