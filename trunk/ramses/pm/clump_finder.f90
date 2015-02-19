3a2b2a6bebaf022fcc5aec492f7fde99fa65e92f
Romain Teyssier <teyssier@pc23.home>
2015-02-19 10:53:34 +0100

    Update rho_only for the clump finder

--- trunk/ramses/pm/clump_finder.f90
+++ trunk/ramses/pm/clump_finder.f90
@@ -1181,3 +1181,330 @@ end subroutine rho_only
 !##############################################################################
 !##############################################################################
 !##############################################################################
+subroutine rho_only_level(ilevel)
+  use amr_commons
+  use pm_commons
+  use hydro_commons
+  use poisson_commons
+  implicit none
+  integer::ilevel
+  !------------------------------------------------------------------
+  ! This routine computes the density field at level ilevel using
+  ! the CIC scheme from particles that are not entirely in
+  ! level ilevel (boundary particles).
+  ! Arrays flag1 and flag2 are used as temporary work space.
+  !------------------------------------------------------------------
+  integer::igrid,jgrid,ipart,jpart,idim,icpu
+  integer::i,ig,ip,npart1
+  real(dp)::dx
+
+  integer,dimension(1:nvector),save::ind_grid,ind_cell
+  integer,dimension(1:nvector),save::ind_part,ind_grid_part
+  real(dp),dimension(1:nvector,1:ndim),save::x0
+
+  ! Mesh spacing in that level
+  dx=0.5D0**ilevel
+
+  ! Loop over cpus
+  do icpu=1,ncpu
+     ! Loop over grids
+     igrid=headl(icpu,ilevel)
+     ig=0
+     ip=0
+     do jgrid=1,numbl(icpu,ilevel)
+        npart1=numbp(igrid)  ! Number of particles in the grid
+        if(npart1>0)then
+           ig=ig+1
+           ind_grid(ig)=igrid
+           ipart=headp(igrid)
+
+           ! Loop over particles
+           do jpart=1,npart1
+              if(ig==0)then
+                 ig=1
+                 ind_grid(ig)=igrid
+              end if
+              ip=ip+1
+              ind_part(ip)=ipart
+              ind_grid_part(ip)=ig
+              if(ip==nvector)then
+                 ! Lower left corner of 3x3x3 grid-cube
+                 do idim=1,ndim
+                    do i=1,ig
+                       x0(i,idim)=xg(ind_grid(i),idim)-3.0D0*dx
+                    end do
+                 end do
+                 do i=1,ig
+                    ind_cell(i)=father(ind_grid(i))
+                 end do
+#ifdef TSC
+                 call tsc_only(ind_cell,ind_part,ind_grid_part,x0,ig,ip,ilevel)
+#else
+                 call cic_only(ind_cell,ind_part,ind_grid_part,x0,ig,ip,ilevel)
+#endif
+                 ip=0
+                 ig=0
+              end if
+              ipart=nextp(ipart)  ! Go to next particle
+           end do
+           ! End loop over particles
+
+        end if
+
+        igrid=next(igrid)   ! Go to next grid
+     end do
+     ! End loop over grids
+
+     if(ip>0)then
+        ! Lower left corner of 3x3x3 grid-cube
+        do idim=1,ndim
+           do i=1,ig
+              x0(i,idim)=xg(ind_grid(i),idim)-3.0D0*dx
+           end do
+        end do
+        do i=1,ig
+           ind_cell(i)=father(ind_grid(i))
+        end do
+#ifdef TSC
+        call tsc_only(ind_cell,ind_part,ind_grid_part,x0,ig,ip,ilevel)
+#else
+        call cic_only(ind_cell,ind_part,ind_grid_part,x0,ig,ip,ilevel)
+#endif
+     end if
+
+  end do
+  ! End loop over cpus
+
+end subroutine rho_only_level
+!##############################################################################
+!##############################################################################
+!##############################################################################
+!##############################################################################
+subroutine cic_only(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
+  use amr_commons
+  use pm_commons
+  use poisson_commons
+  use hydro_commons, ONLY: mass_sph
+  implicit none
+  integer::ng,np,ilevel
+  integer ,dimension(1:nvector)::ind_cell,ind_grid_part,ind_part
+  real(dp),dimension(1:nvector,1:ndim)::x0
+  !------------------------------------------------------------------
+  ! This routine computes the density field at level ilevel using
+  ! the CIC scheme. Only cells that are in level ilevel
+  ! are updated by the input particle list.
+  !------------------------------------------------------------------
+  logical::error
+  integer::j,ind,idim,nx_loc
+  real(dp)::dx,dx_loc,scale,vol_loc
+  ! Grid-based arrays
+  integer ,dimension(1:nvector,1:threetondim),save::nbors_father_cells
+  integer ,dimension(1:nvector,1:twotondim),save::nbors_father_grids
+  ! Particle-based arrays
+  logical ,dimension(1:nvector),save::ok
+  real(dp),dimension(1:nvector),save::mmm
+  real(dp),dimension(1:nvector),save::ttt=0d0
+  real(dp),dimension(1:nvector),save::vol2
+  real(dp),dimension(1:nvector,1:ndim),save::x,dd,dg
+  integer ,dimension(1:nvector,1:ndim),save::ig,id,igg,igd,icg,icd
+  real(dp),dimension(1:nvector,1:twotondim),save::vol
+  integer ,dimension(1:nvector,1:twotondim),save::igrid,icell,indp,kg
+  real(dp),dimension(1:3)::skip_loc
+
+  ! Mesh spacing in that level
+  dx=0.5D0**ilevel
+  nx_loc=(icoarse_max-icoarse_min+1)
+  skip_loc=(/0.0d0,0.0d0,0.0d0/)
+  if(ndim>0)skip_loc(1)=dble(icoarse_min)
+  if(ndim>1)skip_loc(2)=dble(jcoarse_min)
+  if(ndim>2)skip_loc(3)=dble(kcoarse_min)
+  scale=boxlen/dble(nx_loc)
+  dx_loc=dx*scale
+  vol_loc=dx_loc**ndim
+
+  ! Gather neighboring father cells (should be present anytime !)
+  call get3cubefather(ind_cell,nbors_father_cells,nbors_father_grids,ng,ilevel)
+
+  ! Rescale particle position at level ilevel
+  do idim=1,ndim
+     do j=1,np
+        x(j,idim)=xp(ind_part(j),idim)/scale+skip_loc(idim)
+     end do
+  end do
+  do idim=1,ndim
+     do j=1,np
+        x(j,idim)=x(j,idim)-x0(ind_grid_part(j),idim)
+     end do
+  end do
+  do idim=1,ndim
+     do j=1,np
+        x(j,idim)=x(j,idim)/dx
+     end do
+  end do
+
+  ! Gather particle mass
+  do j=1,np
+     mmm(j)=mp(ind_part(j))
+  end do
+
+  ! Gather particle birth epoch
+  if(star)then
+     do j=1,np
+        ttt(j)=tp(ind_part(j))
+     end do
+  endif
+
+  ! Check for illegal moves
+  error=.false.
+  do idim=1,ndim
+     do j=1,np
+        if(x(j,idim)<0.5D0.or.x(j,idim)>5.5D0)error=.true.
+     end do
+  end do
+  if(error)then
+     write(*,*)'problem in cic_only'
+     do idim=1,ndim
+        do j=1,np
+           if(x(j,idim)<0.5D0.or.x(j,idim)>5.5D0)then
+              write(*,*)x(j,1:ndim)
+           endif
+        end do
+     end do
+     stop
+  end if
+
+  ! CIC at level ilevel (dd: right cloud boundary; dg: left cloud boundary)
+  do idim=1,ndim
+     do j=1,np
+        dd(j,idim)=x(j,idim)+0.5D0
+        id(j,idim)=dd(j,idim)
+        dd(j,idim)=dd(j,idim)-id(j,idim)
+        dg(j,idim)=1.0D0-dd(j,idim)
+        ig(j,idim)=id(j,idim)-1
+     end do
+  end do
+
+  ! Compute cloud volumes
+#if NDIM==1
+  do j=1,np
+     vol(j,1)=dg(j,1)
+     vol(j,2)=dd(j,1)
+  end do
+#endif
+#if NDIM==2
+  do j=1,np
+     vol(j,1)=dg(j,1)*dg(j,2)
+     vol(j,2)=dd(j,1)*dg(j,2)
+     vol(j,3)=dg(j,1)*dd(j,2)
+     vol(j,4)=dd(j,1)*dd(j,2)
+  end do
+#endif
+#if NDIM==3
+  do j=1,np
+     vol(j,1)=dg(j,1)*dg(j,2)*dg(j,3)
+     vol(j,2)=dd(j,1)*dg(j,2)*dg(j,3)
+     vol(j,3)=dg(j,1)*dd(j,2)*dg(j,3)
+     vol(j,4)=dd(j,1)*dd(j,2)*dg(j,3)
+     vol(j,5)=dg(j,1)*dg(j,2)*dd(j,3)
+     vol(j,6)=dd(j,1)*dg(j,2)*dd(j,3)
+     vol(j,7)=dg(j,1)*dd(j,2)*dd(j,3)
+     vol(j,8)=dd(j,1)*dd(j,2)*dd(j,3)
+  end do
+#endif
+
+  ! Compute parent grids
+  do idim=1,ndim
+     do j=1,np
+        igg(j,idim)=ig(j,idim)/2
+        igd(j,idim)=id(j,idim)/2
+     end do
+  end do
+#if NDIM==1
+  do j=1,np
+     kg(j,1)=1+igg(j,1)
+     kg(j,2)=1+igd(j,1)
+  end do
+#endif
+#if NDIM==2
+  do j=1,np
+     kg(j,1)=1+igg(j,1)+3*igg(j,2)
+     kg(j,2)=1+igd(j,1)+3*igg(j,2)
+     kg(j,3)=1+igg(j,1)+3*igd(j,2)
+     kg(j,4)=1+igd(j,1)+3*igd(j,2)
+  end do
+#endif
+#if NDIM==3
+  do j=1,np
+     kg(j,1)=1+igg(j,1)+3*igg(j,2)+9*igg(j,3)
+     kg(j,2)=1+igd(j,1)+3*igg(j,2)+9*igg(j,3)
+     kg(j,3)=1+igg(j,1)+3*igd(j,2)+9*igg(j,3)
+     kg(j,4)=1+igd(j,1)+3*igd(j,2)+9*igg(j,3)
+     kg(j,5)=1+igg(j,1)+3*igg(j,2)+9*igd(j,3)
+     kg(j,6)=1+igd(j,1)+3*igg(j,2)+9*igd(j,3)
+     kg(j,7)=1+igg(j,1)+3*igd(j,2)+9*igd(j,3)
+     kg(j,8)=1+igd(j,1)+3*igd(j,2)+9*igd(j,3)
+  end do
+#endif
+  do ind=1,twotondim
+     do j=1,np
+        igrid(j,ind)=son(nbors_father_cells(ind_grid_part(j),kg(j,ind)))
+     end do
+  end do
+
+  ! Compute parent cell position
+  do idim=1,ndim
+     do j=1,np
+        icg(j,idim)=ig(j,idim)-2*igg(j,idim)
+        icd(j,idim)=id(j,idim)-2*igd(j,idim)
+     end do
+  end do
+#if NDIM==1
+  do j=1,np
+     icell(j,1)=1+icg(j,1)
+     icell(j,2)=1+icd(j,1)
+  end do
+#endif
+#if NDIM==2
+  do j=1,np
+     icell(j,1)=1+icg(j,1)+2*icg(j,2)
+     icell(j,2)=1+icd(j,1)+2*icg(j,2)
+     icell(j,3)=1+icg(j,1)+2*icd(j,2)
+     icell(j,4)=1+icd(j,1)+2*icd(j,2)
+  end do
+#endif
+#if NDIM==3
+  do j=1,np
+     icell(j,1)=1+icg(j,1)+2*icg(j,2)+4*icg(j,3)
+     icell(j,2)=1+icd(j,1)+2*icg(j,2)+4*icg(j,3)
+     icell(j,3)=1+icg(j,1)+2*icd(j,2)+4*icg(j,3)
+     icell(j,4)=1+icd(j,1)+2*icd(j,2)+4*icg(j,3)
+     icell(j,5)=1+icg(j,1)+2*icg(j,2)+4*icd(j,3)
+     icell(j,6)=1+icd(j,1)+2*icg(j,2)+4*icd(j,3)
+     icell(j,7)=1+icg(j,1)+2*icd(j,2)+4*icd(j,3)
+     icell(j,8)=1+icd(j,1)+2*icd(j,2)+4*icd(j,3)
+  end do
+#endif
+
+  ! Compute parent cell adress
+  do ind=1,twotondim
+     do j=1,np
+        indp(j,ind)=ncoarse+(icell(j,ind)-1)*ngridmax+igrid(j,ind)
+     end do
+  end do
+
+  ! Update mass density and number density fields
+  do ind=1,twotondim
+     do j=1,np
+        ok(j)=igrid(j,ind)>0
+     end do
+     do j=1,np
+        vol2(j)=mmm(j)*vol(j,ind)/vol_loc
+     end do
+     do j=1,np
+        if(ok(j))then
+           rho(indp(j,ind))=rho(indp(j,ind))+vol2(j)
+        end if
+     end do
+  end do
+
+end subroutine cic_only
