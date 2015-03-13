f0fcdf366dc4ffdf1098188a0de547fbe0b617a2
Romain Teyssier <teyssier@dhcp-94-202.physik.uzh.ch>
2015-03-13 09:36:41 +0100

    Allow for more than 2 billion cells above the threshold in the clump finder.

--- trunk/ramses/pm/clump_merger.f90
+++ trunk/ramses/pm/clump_merger.f90
@@ -349,7 +349,7 @@ subroutine write_clump_properties(to_file)
      end if
      if(saddle_threshold>0)then
         if(ind_halo(jj).EQ.jj+ipeak_start(myid).AND.halo_mass(jj) > mass_threshold*particle_mass)then
-           write(ilun2,'(I8,5(X,1PE18.9E2))')&
+           write(ilun2,'(I10,5(X,1PE18.9E2))')&
                 jj+ipeak_start(myid)&
                 ,peak_pos(jj,1)&
                 ,peak_pos(jj,2)&
