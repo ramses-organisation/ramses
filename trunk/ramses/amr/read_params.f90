a360a54b94943b14ac1d680fa0867ca5e6e171c4
Valentin Perret <perret.valentin@gmail.com>
2016-09-09 15:30:05 +0200

    resolving conflict

--- trunk/ramses/amr/read_params.f90
+++ trunk/ramses/amr/read_params.f90
@@ -38,9 +38,7 @@ subroutine read_params
        & ,xcentre_frame,ycentre_frame,zcentre_frame &
        & ,deltax_frame,deltay_frame,deltaz_frame,movie,zoom_only &
        & ,imovout,imov,tendmov,aendmov,proj_axis,movie_vars,movie_vars_txt &
-       & ,theta_camera,phi_camera,dtheta_camera,dphi_camera,focal_camera &
-       & ,perspective_camera,smooth_frame,shader_frame,tstart_theta_camera,tstart_phi_camera &
-       & ,tend_theta_camera,tend_phi_camera
+       & ,theta_camera,phi_camera,dtheta_camera,dphi_camera,focal_camera,perspective_camera
 
   ! MPI initialization
 #ifndef WITHOUTMPI
