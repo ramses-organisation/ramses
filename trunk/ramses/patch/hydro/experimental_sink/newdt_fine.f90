1f68806c7bad6bfec6721a4cddbb9bedf488dfdb
Romain Teyssier <romain.teyssier@gmail.com>
2017-05-26 12:02:29 +0200

    Cleaning the sink particle algorithm.

--- trunk/ramses/patch/hydro/experimental_sink/newdt_fine.f90
+++ trunk/ramses/patch/hydro/experimental_sink/newdt_fine.f90
@@ -21,13 +21,12 @@ subroutine newdt_fine(ilevel)
   ! This routine also compute the particle kinetic energy.
   !-----------------------------------------------------------
   integer::igrid,jgrid,ipart,jpart,nx_loc
-  integer::npart1,ip,info,isink,ilev,levelmin_isink,limiting_sink
+  integer::npart1,ip,info,isink,ilev
   integer,dimension(1:nvector),save::ind_part
   real(kind=8)::dt_loc,dt_all,ekin_loc,ekin_all,dt_acc_min
   real(dp)::tff,fourpi,threepi2
   real(dp)::aton_time_step,dt_aton,dt_rt
   real(dp)::dx_min,dx,scale,dt_fact,limiting_dt_fact
-  logical::highest_level
 
   if(numbtot(1,ilevel)==0)return
   if(verbose)write(*,111)ilevel
@@ -42,7 +41,7 @@ subroutine newdt_fine(ilevel)
   if(poisson.and.gravity_type<=0)then
      fourpi=4.0d0*ACOS(-1.0d0)
      if(cosmo)fourpi=1.5d0*omega_m*aexp
-     if (sink)then
+     if (sink.and.(mass_sink_direct_force .ge. 0.))then
         tff=sqrt(threepi2/8./fourpi/(rho_max(ilevel)+rho_sink_tff(ilevel)))
      else
         tff=sqrt(threepi2/8./fourpi/rho_max(ilevel))
@@ -118,48 +117,6 @@ subroutine newdt_fine(ilevel)
 #endif
      ekin_tot=ekin_tot+ekin_all
      dtnew(ilevel)=MIN(dtnew(ilevel),dt_all)
-
-
-     ! timestep restrictions due to sink
-     if(sink .and. nsink>0) then
-        ! determine if on highest active level...
-        if (ilevel==nlevelmax)then
-           highest_level=.true.
-        else if (numbtot(1,ilevel+1)==0)then
-           highest_level=.true.
-        else 
-           highest_level=.false.
-        end if
-        
-        if (highest_level)then
-           call compute_accretion_rate(.false.)
-           ! timestep due to sink accretion
-           dt_acc_min=huge(0._dp)
-           do isink=1,nsink
-              
-              levelmin_isink=nlevelmax
-              do ilev=nlevelmax,levelmin,-1
-                 if (level_sink(isink,ilev))levelmin_isink=ilev 
-              end do
-              
-              dt_fact=1.
-              do ilev=levelmin_isink,ilevel-1
-                 dt_fact=dt_fact*nsubcycle(ilev)
-              end do
-              
-              if (dt_acc(isink)/dt_fact<dt_acc_min)then
-                 dt_acc_min=dt_acc(isink)/dt_fact
-                 limiting_sink=isink
-                 limiting_dt_fact=dt_fact
-              end if
-              
-           end do
-           if (myid==1 .and. dt_acc_min<dtnew(ilevel))then
-              write(*,'(A10,2X,F10.6,2X,A20,2X,I10)')'dt_acc/dt',dt_acc_min/dtnew(ilevel),'limited by sink:',limiting_sink
-           end if
-           dtnew(ilevel)=MIN(dtnew(ilevel),dt_acc_min)
-        end if
-     end if
 
   end if
 
