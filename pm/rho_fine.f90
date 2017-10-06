f85f65da65c5b15a52dc36a08dc8116637dbcda7
Corentin Cadiou <contact@cphyc.me>
2017-10-06 14:53:58 +0200

    cache familes

--- pm/rho_fine.f90
+++ pm/rho_fine.f90
@@ -318,6 +318,7 @@ end subroutine rho_from_current_level
 !##############################################################################
 !##############################################################################
 !##############################################################################
+
 subroutine cic_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
   use amr_commons
   use pm_commons
@@ -383,9 +384,10 @@ subroutine cic_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
      end do
   end do
 
-  ! Gather particle mass
+  ! Gather particle mass and family
   do j=1,np
-     if (is_tracer(typep(ind_part(j)))) then
+     fam(j) = typep(ind_part(j))
+     if (is_tracer(fam(j))) then
         mmm(j)=0.0d0
      else
         mmm(j)=mp(ind_part(j))
@@ -556,7 +558,7 @@ subroutine cic_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
   do ind=1,twotondim
 
      do j=1,np
-        ok(j)=(igrid(j,ind)>0).and.is_not_tracer(typep(ind_part(j)))
+        ok(j)=(igrid(j,ind)>0).and.is_not_tracer(fam(j))
      end do
 
      do j=1,np
@@ -572,7 +574,7 @@ subroutine cic_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
      else if(ilevel>cic_levelmax)then
         do j=1,np
            ! check for non-DM (and non-tracer)
-           if ( ok(j) .and. is_not_DM(typep(ind_part(j))) ) then
+           if ( ok(j) .and. is_not_DM(fam(j)) ) then
               rho(indp(j,ind))=rho(indp(j,ind))+vol2(j)
            end if
         end do
@@ -581,7 +583,7 @@ subroutine cic_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
      if(ilevel==cic_levelmax)then
         do j=1,np
            ! check for DM
-           if ( ok(j) .and. is_DM(typep(ind_part(j))) ) then
+           if ( ok(j) .and. is_DM(fam(j)) ) then
               rho_top(indp(j,ind))=rho_top(indp(j,ind))+vol2(j)
            end if
         end do
@@ -598,20 +600,20 @@ subroutine cic_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
         end do
      endif
 
-     ! Remove massive dark matter particle
+     ! Keep only DM particle with a mass below the mass cut
      if(mass_cut_refine>0.0)then
         do j=1,np
-           if ( is_DM(typep(ind_part(j))) ) then
-              ok(j)=ok(j).and.mmm(j)<mass_cut_refine
+           if ( is_DM(fam(j)) ) then
+              ok(j)=ok(j) .and. mmm(j) < mass_cut_refine
            endif
         end do
      endif
 
-     ! For low mass baryon particles
+     ! Rescale the mass by mass_sph for baryon particles
      if(star)then
         do j=1,np
-           if ( is_not_DM(typep(ind_part(j))) ) then
-              vol2(j)=vol2(j)*mmm(j)/mass_sph
+           if ( is_not_DM(fam(j)) ) then
+              vol2(j) = vol2(j)*mmm(j)/mass_sph
            endif
         end do
      endif
@@ -624,7 +626,7 @@ subroutine cic_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
         end do
      else if(ilevel>=cic_levelmax)then
         do j=1,np
-           if ( ok(j) .and. is_not_DM(typep(ind_part(j))) ) then
+           if ( ok(j) .and. is_not_DM(fam(j)) ) then
               phi(indp(j,ind))=phi(indp(j,ind))+vol2(j)
            end if
         end do
@@ -634,7 +636,7 @@ subroutine cic_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
      ! by setting particle number density above m_refine(ilevel)
      if(sink_refine)then
         do j=1,np
-           if ( is_cloud(typep(ind_part(j))) ) then
+           if ( is_cloud(fam(j)) ) then
               ! if (direct_force_sink(-1*idp(ind_part(j))))then
               phi(indp(j,ind))=phi(indp(j,ind))+m_refine(ilevel)
               ! endif
@@ -1199,9 +1201,10 @@ subroutine tsc_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
      end do
   end do
 
-  ! Gather particle mass
+  ! Gather particle mass & type
   do j=1,np
-     if (is_tracer(typep(ind_part(j)))) then
+     fam(j) = typep(ind_part(j))
+     if (is_tracer(fam(j))) then
         mmm(j)=0.
      else
         mmm(j)=mp(ind_part(j))
@@ -1227,11 +1230,6 @@ subroutine tsc_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
         ttt(j)=tp(ind_part(j))
      end do
   endif
-
-  ! ! Gather particle type
-  ! do j = 1, np
-  !    fam(j) = typep(ind_part(j))
-  ! end do
 
   ! Check for illegal moves
   abandoned(1:np)=.false.
@@ -1417,7 +1415,7 @@ subroutine tsc_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
      else if(ilevel>cic_levelmax) then
         do j=1,np
            ! if(ok(j).and.(ttt(j).ne.0d0).and.(.not.abandoned(j))) then
-           if ( ok(j) .and. is_not_DM(typep(ind_part(j))) .and. (.not.abandoned(j)) ) then
+           if ( ok(j) .and. is_not_DM(fam(j)) .and. (.not.abandoned(j)) ) then
               rho(indp(j,ind))=rho(indp(j,ind))+vol2(j)
            end if
         end do
@@ -1426,7 +1424,7 @@ subroutine tsc_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
      if(ilevel==cic_levelmax)then
         do j=1,np
            ! if(ok(j).and.(ttt(j)==0d0).and.(.not.abandoned(j)))then
-           if ( ok(j) .and. is_DM(typep(ind_part(j))) .and. (.not.abandoned(j)) ) then
+           if ( ok(j) .and. is_DM(fam(j)) .and. (.not.abandoned(j)) ) then
               rho_top(indp(j,ind))=rho_top(indp(j,ind))+vol2(j)
            end if
         end do
@@ -1451,7 +1449,7 @@ subroutine tsc_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
      if(mass_cut_refine>0.0) then
         do j=1,np
            ! if(ttt(j)==0d0.and.(.not.abandoned(j))) then
-           if ( is_DM(typep(ind_part(j))) .and. (.not.abandoned(j)) ) then
+           if ( is_DM(fam(j)) .and. (.not.abandoned(j)) ) then
               ok(j)=ok(j).and.mmm(j)<mass_cut_refine
            endif
         end do
@@ -1461,7 +1459,7 @@ subroutine tsc_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
      if(star) then
         do j=1,np
            ! if(ttt(j).ne.0.0.and.(.not.abandoned(j))) then
-           if ( is_not_DM(typep(ind_part(j))) .and. (.not.abandoned(j)) ) then
+           if ( is_not_DM(fam(j)) .and. (.not.abandoned(j)) ) then
               vol2(j)=vol2(j)*mmm(j)/mass_sph
            endif
         end do
@@ -1476,7 +1474,7 @@ subroutine tsc_amr(ind_cell,ind_part,ind_grid_part,x0,ng,np,ilevel)
      else if(ilevel>=cic_levelmax) then
         do j=1,np
            ! if(ok(j).and.(ttt(j).ne.0d0).and.(.not.abandoned(j))) then
-           if ( ok(j) .and. is_not_DM(typep(ind_part(j))) .and. (.not.abandoned(j)) ) then
+           if ( ok(j) .and. is_not_DM(fam(j)) .and. (.not.abandoned(j)) ) then
               phi(indp(j,ind))=phi(indp(j,ind))+vol2(j)
            end if
         end do
