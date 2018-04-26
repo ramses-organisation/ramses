2eda2fd76ce8d42db45dfc463da3d55e7d9de6f3
Corentin Cadiou <contact@cphyc.me>
2018-04-26 22:39:57 +0200

    Compiling in 3D

--- tests/build.default.sh
+++ tests/build.default.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd bin
-sed 's/^F90 =.*$/F90 = mpif90 -frecord-marker=4 -ffree-line-length-none -fbacktrace -g -O -fbounds-check -Wuninitialized -Wall/g' Makefile | \
+cat Makefile |\
+    sed 's/NDIM = 1/NDIM = 3/g' | \
+    sed 's/^F90 =.*$/F90 = mpif90 -frecord-marker=4 -ffree-line-length-none -fbacktrace -g -O -fbounds-check -Wuninitialized -Wall/g'| \
     sed 's/^FFLAGS =.*$/FFLAGS = -x f95-cpp-input -ffpe-trap=zero,underflow,overflow,invalid -finit-real=nan  $(DEFINES)/g' > Makefile.default
 make -f Makefile.default clean
 
