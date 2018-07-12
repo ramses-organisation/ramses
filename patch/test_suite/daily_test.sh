3e9827a0b8227145d345691430ed622c8eaa5a53
Pawel Biernacki <biernackip@yahoo.com>
2018-07-12 17:18:23 +0200

    Attempt at fixing/rewriting the wikipage with autotests

--- patch/test_suite/daily_test.sh
+++ patch/test_suite/daily_test.sh
@@ -2,7 +2,7 @@
 
 ############################################################################
 # Ramses Daily Build Script
-# 
+#
 # Neil Vaytet (ENS Lyon) - 07/2014 - neil.vaytet@ens-lyon.fr
 #
 # This script runs the RAMSES test suite at regular time intervals.
@@ -152,122 +152,33 @@ while true ; do
 
         # Run ramses test suite
         ./run_test_suite.sh >> $LOGFILE;
-        
+
         # Go to wiki directory
         cd "${WIKIDIR}/${WIKISTOREDIR}";
         cp ${RAMSESDIR}/test_results.pdf ${DATE}.pdf;
         cp ${RAMSESDIR}/test_suite.log ${DATE}.log;
 
         if $UPDATEWIKI ; then
-        
+
             # Update wiki page ================================================
             cd ${WIKIDIR};
-            
-            # Pull latest wiki ================================================
-	    git pull;
 
-            # Number of lines in file
-            nlines=$(wc -l ${WIKIFILE} | cut -d ' ' -f 1);
-            
-            # Check if year already exists
-            yearcheck=$(grep "## ${YEAR}" ${WIKIFILE});
-            if [ ${#yearcheck} -eq 0 ] ; then
-                oldyear=$(($YEAR - 1));
-                sed -i "s/## ${oldyear}/## ${YEAR}\n\n## ${oldyear}/g" ${WIKIFILE};
-                # Update number of lines
-                nlines=$(wc -l ${WIKIFILE} | cut -d ' ' -f 1);
-            fi
-            
-            # Check if month already exists
-            monthcheck=$(grep "${YEARMONTH}" ${WIKIFILE});
-            if [ ${#monthcheck} -eq 0 ] ; then
-                # Generate new month group
-                m=$((($MONTHNOZERO - 1) / 4));
-                monthlist="||";
-                monthhead="[//]: # (";
-                for ((i=0;i<4;i++)); do
-                   j=$(($m * 4 + $i));
-                   monthlist="${monthlist} ${months[$j]} |";
-                   imonth=$(($j + 1));
-                   if [ $imonth -lt 10 ] ; then
-                      imonth="0${imonth}";
-                   fi
-                   monthhead="${monthhead} ${YEAR}-${imonth} ";
-                done
-                monthhead="${monthhead})";
-                
-                lnumber=$(grep -n "## ${YEAR}" ${WIKIFILE} | head -1 | cut -d ':' -f 1);
-                fileend=$(($nlines - $lnumber));
-                
-                head -n $lnumber ${WIKIFILE} > tempfile;
-                echo >> tempfile;
-                echo "$monthhead" >> tempfile;
-                echo "$monthlist" >> tempfile;
-                echo "$hline" >> tempfile;
-                for ((i=0;i<31;i++)); do
-                    j=$(($i + 1));
-                    echo "| ${j} | | | | |" >> tempfile;
-                done
-                echo >> tempfile;
-                tail -n ${fileend} ${WIKIFILE} >> tempfile;
-                cp tempfile ${WIKIFILE};
-                # Update number of lines
-                nlines=$(wc -l ${WIKIFILE} | cut -d ' ' -f 1);
-            fi
-            
-            # Identify line where to insert new results
-            lnumber=$(grep -n ${YEARMONTH} ${WIKIFILE} | head -1 | cut -d ':' -f 1);
-            
-            line1=$(($lnumber + $DAYNOZERO + 2));
-            line2=$(($line1 - 1));
-            line3=$(($nlines - $line1));
-            
-            theline=$(head -n $line1 ${WIKIFILE} | tail -1);
-            
-            # Extract separate columns from line
-            for ((i=0;i<5;i++)); do
-               j=$(($i + 2));
-               segment[i]=$(echo $theline | cut -d '|' -f $j);
-            done
-            
-            # Generate image and commit string
-            failcheck=$(grep -i fail ${WIKISTOREDIR}/${DATE}.log);
-            faillength=${#failcheck};
-            if [ $faillength -eq 0 ] ; then
-                image="![ok](ok.png)";
-            else
-                image="![fail](fail.png)";
-            fi
-            URL="${COMMIT_URL}${commit}";
-            
-            # Identify segment
-            imonth=$(((($MONTHNOZERO - 1) % 4) + 1));
-            segment[$imonth]=" [pdf](${WIKISTOREDIR}/${DATE}.pdf) [log](${WIKISTOREDIR}/${DATE}.log) [${commit:0:7}](${URL}) ${image} ";
-            
-            newline="|";
-            for ((i=0;i<5;i++)); do
-               newline="${newline}${segment[i]}|";
-            done
-            
-            # Write to new file
-            head -n $line2 ${WIKIFILE} > tempfile;
-            echo "$newline" >> tempfile;
-            tail -n $line3 ${WIKIFILE} >> tempfile;
-            
-            # Update original file
-            cp tempfile ${WIKIFILE};
-            rm tempfile;
-            
+            # Pull latest wiki ================================================
+	        git pull;
+
+            # Call Python script to update the wikifile
+            python $RAMSESDIR/update_testpage.py
+
             # Upload results to bitbucket
             git add ${WIKIFILE} ${WIKISTOREDIR}/${DATE}.log ${WIKISTOREDIR}/${DATE}.pdf >> $LOGFILE;
             git commit -m "Test results for date: ${DATE}" >> $LOGFILE;
             git push origin master >> $LOGFILE;
-            
+
         fi
 
         # Return to base directory
         cd $SRC;
-        
+
         # Update next backup time
         TNEXTBACKUP=$(date -d "1970-01-01 UTC ${TNEXTBACKUP} second ${frequencies[0]} year ${frequencies[1]} month ${frequencies[2]} day ${frequencies[3]} hour ${frequencies[4]} minute ${frequencies[5]} second" +%s);
         TNEXT=$(date -d "1970-01-01 UTC ${TNEXTBACKUP} second");
---
+++ patch/test_suite/update_testpage.py
@@ -0,0 +1,116 @@
+import glob
+from datetime import date, timedelta
+
+
+MonthsDict = {'Jan': 1, 'Feb': 2, 'Mar': 3,
+              'Apr': 4, 'May': 5, 'Jun': 6,
+              'Jul': 7, 'Aug': 8, 'Sep': 9,
+              'Oct': 10, 'Nov': 11, 'Dec': 12}
+
+
+def wiki_entry(date, commit, success):
+    """Format extracted info as a Markdown table entry"""
+
+    # format wiki entry
+    pdf_string = ('[pdf](daily_tests/{year}-{month:02}-{day:02}.pdf)'
+                  .format(year=date.year, month=date.month, day=date.day))
+    log_string = ('[log](daily_tests/{year}-{month:02}-{day:02}.log)'
+                  .format(year=date.year, month=date.month, day=date.day))
+    commit_string = ('[{short_hash}](https://bitbucket.org/rteyssie/ramses/commits/{long_hash})'
+            .format(short_hash=commit[0:7], long_hash=commit))
+    if success:
+        success_string = '![ok](ok.png)'
+    else:
+        success_string = '![fail](fail.png)'
+
+    return '| '+str(date)+' | '+pdf_string+' | '+log_string+' | '+commit_string+' | '+success_string+' |\n'
+
+
+def extract_test_info(filename):
+    """Extracts crucial test info from the log file"""
+
+    with open(filename) as infile:
+        passed = True
+        last_commit = 7*'?'
+
+        # figure out the date from the filename
+        compile_date = date(int(filename[12:16]),
+                            int(filename[17:19]),
+                            int(filename[20:22]))
+
+        for line in infile:
+            if 'compile' in line:
+                    passed = True
+            # find if commit info exists
+            if 'commit' in line.split(' '):
+                last_commit = line.split('=')[-1][1:-1]
+
+            # if any of the tests failed, then all failed
+            if 'failed' in line.split(' '):
+                passed = False
+
+    return compile_date, last_commit, passed
+
+
+def rebuild_wiki(logs):
+    """Rebuilts the AutoTests.md wiki file from scratch"""
+
+    dates = []
+    commits = []
+    successes = []
+
+    # process logs
+    for logname in logs:
+        compile_date, last_commit, passed = extract_test_info(logname)
+
+        dates.append(compile_date)
+        commits.append(last_commit)
+        successes.append(passed)
+
+    # buld a new wiki md file
+    entries = []
+    with open('AutoTests.md', 'w') as wikifile:
+        wikifile.write('# RAMSES Daily Test\n')
+        wikifile.write('\n')
+        wikifile.write('This page contains the test results from the RAMSES test suite which is run daily. The test pipeline was provided by Neil Vaytet with fixes/expansions by Pawel Biernacki.\n')
+        wikifile.write('\n')
+        wikifile.write('| date | pdf | log | commit | result |\n')
+        wikifile.write('| ---:|:---:|:---:|:---:|:---:|\n')
+        # table entries
+        for i in xrange(len(dates)):
+            entry = wiki_entry(dates[~i], commits[~i], successes[~i])
+            wikifile.write(entry)
+
+    return
+
+
+def add_last_entry(logfile):
+    """Finds last logfile, extracts the test info and adds it to the wiki"""
+
+    # gather info on the last test
+    when, commit, passed = extract_test_info(logfile)
+
+    # read the content of wiki
+    with open('AutoTests.md', 'w') as wikifile:
+        wiki_contents = wikifile.readlines()
+
+    # insert the latest test after the header
+    wiki_contents.insert(6, wiki_entry(when, commit, passed))
+
+    # write the updated content of the wiki
+    with open('AutoTests.md', 'w') as wikifile:
+        wiki_contents = ''.join(wiki_contents)
+        wikifile.write(wiki_contents)
+
+
+def main():
+
+    # gather all the log files
+    logs =  glob.glob('daily_tests/201*.log')
+
+    # rebuild_wiki(logs)
+    add_last_entry(logs[-1])
+
+
+if __name__ == '__main__':
+    main()
