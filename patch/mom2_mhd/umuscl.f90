8afaaa5c049b1352a526f26b6a6a171afb83acd9
Yuankang Liu <yliu@daint102.login.cscs.ch>
2019-09-10 17:58:07 +0200

    Snleft & Snright Bug Fixes

--- patch/mom2_mhd/umuscl.f90
+++ patch/mom2_mhd/umuscl.f90
@@ -1370,7 +1370,7 @@ subroutine cmpflxm(qm,im1,im2,jm1,jm2,km1,km2, &
 
   ! local variables
   integer ::i, j, k, l, xdim
-  real(dp),dimension(1:nvector),save::snleft,snright
+  real(dp)::snleft,snright
   real(dp),dimension(1:nvar)::qleft,qright
   real(dp),dimension(1:nvar+1)::fgdnv
   real(dp)::zero_flux, bn_mean, entho
