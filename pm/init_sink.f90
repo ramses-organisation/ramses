415fd2b44083d5ade8c6f0c57d40a58ec1f3011b
rteyssie <romain.teyssier@gmail.com>
2020-05-11 17:59:33 +0200

    Fix uninitialized variables in init_sink

--- pm/init_sink.f90
+++ pm/init_sink.f90
@@ -169,8 +169,8 @@ subroutine init_sink
         vel_gas(nsink,3)=svg3
         new_born(nsink)=.false. ! this is a restart
         msmbh(nsink)=sm2
-        vsold(nsink,1:ndim,slevel)=vsink(nsink,1ndim)
-        vsnew(nsink,1:ndim,slevel)=vsink(nsink,1ndim)
+        vsold(nsink,1:ndim,slevel)=vsink(nsink,1:ndim)
+        vsnew(nsink,1:ndim,slevel)=vsink(nsink,1:ndim)
      end do
 104  continue
      sinkint_level=slevel
@@ -255,8 +255,8 @@ subroutine init_sink
         tsink(nsink)=t
         new_born(nsink)=.false.
         msmbh(nsink)=sm2
-        vsold(nsink,1:ndim,slevel)=vsink(nsink,1ndim)
-        vsnew(nsink,1:ndim,slevel)=vsink(nsink,1ndim)
+        vsold(nsink,1:ndim,slevel)=vsink(nsink,1:ndim)
+        vsnew(nsink,1:ndim,slevel)=vsink(nsink,1:ndim)
      end do
      sinkint_level=levelmin
 103  continue
