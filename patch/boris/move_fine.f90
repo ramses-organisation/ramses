d5d228ea7d8de0dd52ed043e4812f10cf4a0aaa5
Eric Moseley <moseley@princeton.edu>
2021-02-22 08:10:36 -0800

    changed constant values of ctm, ts to variables

--- patch/boris/move_fine.f90
+++ patch/boris/move_fine.f90
@@ -164,7 +164,7 @@ end subroutine move_fine_static
 !#########################################################################
 subroutine move1(ind_grid,ind_part,ind_grid_part,ng,np,ilevel)
   use amr_commons
-  use amr_parameters !, ONLY:charge_to_mass,t_stop
+  !use amr_parameters !, ONLY:charge_to_mass,t_stop ERM
   use pm_commons
   use poisson_commons
   use hydro_commons, ONLY: uold,smallr,nvar ! ERM: want nvar?
@@ -182,8 +182,8 @@ subroutine move1(ind_grid,ind_part,ind_grid_part,ng,np,ilevel)
   logical::error
   integer::i,j,ind,idim,nx_loc,isink
   real(dp)::dx,dx_loc,scale,vol_loc
-  real(dp)::ctm=1.15D3 ! ERM: recommend 1.15D3
-  real(dp)::ts=2.2D-1 !ERM: recommend 2.2D-1
+  real(dp)::ctm=charge_to_mass! ERM: recommend 1.15D3
+  real(dp)::ts=t_stop !ERM: recommend 2.2D-1
 
   ! Grid-based arrays
   integer ,dimension(1:nvector),save::father_cell
