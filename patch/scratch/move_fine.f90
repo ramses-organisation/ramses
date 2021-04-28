2ae104b60239bce47daaa86c30b0329fe700f8e7
Eric Moseley <moseley@princeton.edu>
2021-04-27 17:42:16 -0700

    found the bug

--- patch/scratch/move_fine.f90
+++ patch/scratch/move_fine.f90
@@ -577,17 +577,30 @@ subroutine move1(ind_grid,ind_part,ind_grid_part,ng,np,ilevel)
      end do
   end if
 
-  write(*,*)'!!!!!!!!!!HEYYYYY!!!!!!!!!!!'
-
-  if(boris.or.tracer)then
+  if(boris.or.tracer)then!Various fields interpolated to particle positions
+  !   !Gather 3-velocity and 3-magnetic field
+    uu(1:10,1:ndim)=0.0D0
+    bb(1:10,1:ndim)=0.0D0
+       do ind=1,twotondim
+          do idim=1,ndim
+            do index_part=1,10
+             do j=1,np
+               if(idp(ind_part(j)).EQ.index_part)then
+                uu(index_part,idim)=uu(index_part,idim)+uold(indp(j,ind),idim+1)/max(uold(indp(j,ind),1),smallr)*vol(j,ind)
+                bb(index_part,idim)=bb(index_part,idim)+0.5D0*(uold(indp(j,ind),idim+5)+uold(indp(j,ind),idim+nvar))*vol(j,ind)
+               endif
+             end do
+            end do
+          end do
+       end do
      do index_part=1,10
         do j=1,np
            if(idp(ind_part(j)).EQ.index_part)then
               write(25+myid,*)t-dtnew(ilevel),idp(ind_part(j)),& ! Old time
                    & xp(ind_part(j),1),xp(ind_part(j),2),xp(ind_part(j),3),& ! Old particle position
-                   & vp(ind_part(j),1),vp(ind_part(j),2),vp(ind_part(j),3) ! Old particle velocity
-                   ! &  uu(j,1),uu(j,2),uu(j,3),& ! Old fluid velocity
-                   ! &  bb(j,1),bb(j,2),bb(j,3),& ! Old magnetic field.
+                   & vp(ind_part(j),1),vp(ind_part(j),2),vp(ind_part(j),3),& ! Old particle velocity
+                   &  uu(index_part,1),uu(index_part,2),uu(index_part,3),& ! Old fluid velocity
+                   &  bb(index_part,1),bb(index_part,2),bb(index_part,3),& ! Old magnetic field.
                    ! & new_vp(j,1),new_vp(j,2),new_vp(j,3) ! NEW particle velocity (for comparison)
            end if
         end do
