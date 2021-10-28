0bc885cf26115fc114aa5143773454197827b553
Eric Moseley <moseley@princeton.edu>
2021-10-28 08:47:25 -0700

    testing CN drag

--- patch/tsc-scratch/move_fine.f90
+++ patch/tsc-scratch/move_fine.f90
@@ -1404,9 +1404,9 @@ subroutine NewDragKick(nn,dt,indp,ok,vol,nu,big_v,big_w,v,xtondim)
         do idim=1,3
           vc=(uold(indp(i,ind),idim+ivar_dust)&
           &+uold(indp(i,ind),idim+1))/(den_dust+den_gas)
-          un=vc(idim)-mu*big_w(i,ind,idim)/(1+mu)
+          un=vc-mu*big_w(i,ind,idim)/(1+mu)
           big_w(i,ind,idim)=(1-nuj*dt*0.5)*big_w(i,ind,idim)/(1+nuj*dt*0.5)
-          up=vc(idim)&
+          up=vc&
           &-mu*big_w(i,ind,idim)/(1+mu)
 
           big_v(i,ind,idim)=(1-nu(i)*dt*0.5)*big_v(i,ind,idim)/(1+nu(i)*dt*0.5)&
