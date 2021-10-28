a88993ff648b1cb1076b540bbee761adf33a0662
Eric Moseley <moseley@princeton.edu>
2021-10-28 13:58:26 -0700

    testing drag

--- patch/tsc-scratch/move_fine.f90
+++ patch/tsc-scratch/move_fine.f90
@@ -1398,21 +1398,31 @@ subroutine NewDragKick(nn,dt,indp,ok,vol,nu,big_v,big_w,v,xtondim)
   integer ,dimension(1:nvector,1:xtondim)::indp
   real(dp),dimension(1:nvector,1:ndim) ::v ! grain velocity
   real(dp),dimension(1:nvector,1:xtondim,1:ndim) ::big_v,big_w
-  real(dp) ::den_dust,den_gas,mu,nuj,up,un,vc,twopi
+  real(dp) ::den_dust,den_gas,mu,nuj,up,un,twopi
   integer ::i,j,ind,idim! Just an index
-  real(dp),dimension(1:3) ::utemp
+  real(dp),dimension(1:3) ::utemp,vc
   ivar_dust=9
+  twopi=2.*3.1415926
 
   do ind=1,xtondim
      do i=1,nn
         den_gas=unew(indp(i,ind),1)
-
+        do idim=1,3
+          vc(idim)=(unew(indp(i,ind),idim+1))/(den_gas)
+        end do
         !den_dust=uold(indp(i,ind),ivar_dust)
         !mu=den_dust/max(den_gas,smallr)
         !nuj=(1.+mu)*unew(indp(i,ind),ivar_dust)/max(uold(indp(i,ind),ivar_dust),smallr)
+        big_v(i,ind,1)=vc(1)*nu(i)*nu(i)/(nu(i)*nu(i)+twopi*twopi)+&
+        &vc(2)*nu(i)*twopi/(nu(i)*nu(i)+twopi*twopi)&
+        &-exp(-nu(i)*t)*nu(i)*nu(i)/(twopi*twopi+nu(i)*nu(i))
+        big_v(i,ind,2)=vc(2)*nu(i)*nu(i)/(nu(i)*nu(i)+twopi*twopi)-&
+        &vc(1)*nu(i)*twopi/(nu(i)*nu(i)+twopi*twopi)+&
+        &exp(-nu(i)*t)*twopi*nu(i)/(twopi*twopi+nu(i)*nu(i))
+        big_v(i,ind,3)=1.0
         do idim=1,3
           vc=(unew(indp(i,ind),idim+1))/(den_gas)
-          big_v(i,ind,idim)=vc
+          big_v(i,ind,idim)=
         end do
       end do
   end do
