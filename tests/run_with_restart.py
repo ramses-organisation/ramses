d409267bafd31185a30508471c7b2966f0c3ea51
Noe Brucy <noe.brucy@ens-lyon.fr>
2025-07-10 14:20:46 +0200

    improve names and add doc

--- tests/run_with_restart.py
+++ tests/run_with_restart.py
@@ -26,20 +26,30 @@ Where:
 The script uses the f90nml library to read and write Fortran namelist files.
 """
 
-def apply_output_factor(nml, factor, keep_last_output=False):
+def apply_output_factor(nml, factor, add_extra_output_time=False):
     """
     Apply a factor to the output time in the namelist.
     This function modifies the namelist in place.
+    
+    Parameters
+    ---------
+    nml: namelist
+        Namelist object to modify in place
+    factor: float
+        Factor to apply to the existing output
+    add_extra_output_time: bool
+        Add the modified output time after the existing one instead of 
+        modifying it.
     """
     if "tout" in nml["output_params"]:
         tout = nml["output_params"]["tout"]
-        if keep_last_output:
+        if add_extra_output_time:
             nml["output_params"]["tout"] = [tout, tout * factor]
         else:
             nml["output_params"]["tout"] = tout * factor
     elif "aout" in nml["output_params"]:
         aout = nml["output_params"]["aout"]
-        if keep_last_output:
+        if add_extra_output_time:
             nml["output_params"]["aout"] = [aout, aout * factor]
         else:
             nml["output_params"]["aout"] = aout * factor
@@ -91,7 +101,7 @@ def step_2(test_name):
     if "noutput" in nml["output_params"]:
         nml["run_params"]["nrestart"] = 2
         nml["output_params"]["noutput"] = 2
-        nml = apply_output_factor(nml, 2, keep_last_output=True)
+        nml = apply_output_factor(nml, 2, add_extra_output_time=True)
     else:
         # Find the last output time
         last_output = int(glob.glob("output_*")[-1].split("_")[-1])
