794f4344ff622b434eb74c5f4e61d34e97b517dd
Tine Colman <tinecolman@hotmail.com>
2025-09-09 15:02:17 +0200

    remove flto as a default compiler flag to fix compilation on mac

--- bin/clusters.mk
+++ bin/clusters.mk
@@ -12,16 +12,33 @@ endif
 #
 ifeq ($(MACHINE),discoverer)
    MPIF90 = mpif90
+   ifeq ($(COMPILER),GNU)
+      FFLAGS_OPT += -march=native
+      # link-time optimization accross files
+      LDFLAGS += -flto -fwhole-program
+      FFLAGS_OPT += -flto -fwhole-program
+   endif
 endif
 
 #
 ifeq ($(MACHINE),karolina)
    MPIF90 = mpif90
+   ifeq ($(COMPILER),GNU)
+      FFLAGS_OPT += -march=native
+      # link-time optimization accross files
+      LDFLAGS += -flto -fwhole-program
+      FFLAGS_OPT += -flto -fwhole-program
+   endif
 endif
 
 #
 ifeq ($(MACHINE),leonardo)
    MPIF90 = mpiifort
+   ifeq ($(COMPILER),INTEL)
+      # link-time optimization and inlining accross files
+      LDFLAGS += -ipo -inline-forceinline
+      FFLAGS_OPT += -ipo -inline-forceinline
+   endif
 endif
 
 #
@@ -29,11 +46,19 @@ ifeq ($(MACHINE),lumi)
    MPIF90 = ftn
    # mismatch flag needed to circumvent MPI_ALLREDUCE error
    FFLAGS_BASE += -fallow-argument-mismatch
+   # link-time optimization accross files
+   LDFLAGS += -flto -fwhole-program
+   FFLAGS_OPT += -flto -fwhole-program
 endif
 
 #
 ifeq ($(MACHINE),marenostrum)
    MPIF90 = mpif90
+   ifeq ($(COMPILER),INTEL)
+      # link-time optimization and inlining accross files
+      LDFLAGS += -ipo -inline-forceinline
+      FFLAGS_OPT += -ipo -inline-forceinline
+   endif
 endif
 
 #
@@ -41,10 +66,19 @@ ifeq ($(MACHINE),meluxina)
    MPIF90 = mpif90
    ifeq ($(COMPILER),GNU)
       FFLAGS_OPT += -march=native
+      # link-time optimization accross files
+      LDFLAGS += -flto -fwhole-program
+      FFLAGS_OPT += -flto -fwhole-program
    endif
 endif
 
 #
 ifeq ($(MACHINE),vega)
    MPIF90 = mpif90
+   ifeq ($(COMPILER),GNU)
+      FFLAGS_OPT += -march=native
+      # link-time optimization accross files
+      LDFLAGS += -flto -fwhole-program
+      FFLAGS_OPT += -flto -fwhole-program
+   endif
 endif
