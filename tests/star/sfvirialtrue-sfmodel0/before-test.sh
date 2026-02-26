cc05cb04f75e47f4933afffb49944dbf7b933c83
pocvirk <57708653+pocvirk@users.noreply.github.com>
2026-02-26 13:42:13 +0100

    changed IC repo location to ramses.cnrs.fr

--- tests/star/sfvirialtrue-sfmodel0/before-test.sh
+++ tests/star/sfvirialtrue-sfmodel0/before-test.sh
@@ -1,6 +1,6 @@
 if [ ! -d "dwarf_galaxy" ]; then
     echo "dwarf_galaxy ICs directory does not exist, dowloading it..."
-    wget --timeout=10 --tries=3 --content-disposition https://seafile.unistra.fr/f/b294343dac9c4ce7b22e/?dl=1 --no-check-certificate
+    wget --timeout=10 --tries=3 --content-disposition https://ramses.cnrs.fr/wp-content/uploads/2026/02/dwarf_galaxy.tar  --no-check-certificate
     tar -xvf dwarf_galaxy.tar
     rm dwarf_galaxy.tar
 
