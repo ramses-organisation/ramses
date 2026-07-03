f5fa4b5e22799276358e5ed0c779031a55103716
Tine Colman <tinecolman@hotmail.com>
2026-07-03 16:29:32 +0200

    fix compilation bug after removing cloud_module

--- hydro/read_hydro_params.f90
+++ hydro/read_hydro_params.f90
@@ -34,9 +34,7 @@ subroutine read_hydro_params(nml_ok)
 #if NENER>0
        & ,prad_region &
 #endif
-       & ,omega_b,alpha_dense_core,beta_dense_core,crit_dense_core,delta_rho,theta_mag,mass_c,Mach &
-       & ,rap,cont,ff_sct,ff_rt,ff_act,ff_vct,bb_test &
-       & ,contrast,Mach,uniform_bmag,r0_box
+       & ,omega_b,alpha_dense_core,beta_dense_core,crit_dense_core,delta_rho,theta_mag,mass_c,Mach
 
   ! Hydro parameters
   namelist/hydro_params/gamma,courant_factor,smallr,smallc &
