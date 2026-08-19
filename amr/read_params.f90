ramses-organisation/ramses

commit da1ae65e3357400f5c329382bb67cd65ec7ad9f8

Update to the restart mechanism for tests + consistency checks for output_params (#288)
## Improvements to the mechanism that is testing restarts

1) Rewrite the restart script to allow for a wider range of output
parameters in test namelists. This should fix typical issues encountered
when adding new tests.

2) Add a test config option that will run individual tests using the
restart mechanism by default. This option is added as a second line in
config.txt, where the compilation parameters are specified. This option
has been enabled for most tests.

Restarts are now tested **by default**.

## Improvements for output_params handling

Having a wider variety of options to tests restarts revealed some issues
with combinations of output namelist params. Particularly, the
output_params of the sfvirialtrue-sfmodel0 created a nonsense tout list.
This resulted in no output being written at tend. To avoid this
behavior, I've added more consistency checks on the use of `tout`,
`aout`, `tend` and `aout`:
* tend/aend should be >= to the last tout/aout given
* the values of tout/aout should be increasing

Since noutput is calculated automatically (introduced in a previous PR),
the namelist parameter `noutput` is now deprecated, to avoid users
providing a wrong value. If provided, it will simply be reset and print
a warning in the log.

 > [!WARNING]
`noutput` is now deprecated. Setting it in the namelist will not do
anything.

The dice patch has been updated, since the SF/FB tests were affected.
The sfvirialtrue-sfmodel0 test has been updated to properly use
delta_tout for testing purposes.

## Other

* Fixed a formatting bug in the test suite log where at the end
everything would be printed on one super long line. This caused python
errors thrown by visu_ramses or the test plotting scripts to become
difficult to read.

* Fixed the tracer particle test which had very large tolerances and no
check on the output time. This caused the crash at restart to go

amr/read_params.f90
@@ -464,14 +464,13 @@ subroutine read_output_params(namelist_unit,nml_ok)
    implicit none
    integer,intent(in)::namelist_unit
    logical,intent(inout)::nml_ok
-   integer::nml_err
+   integer::nml_err,i
    real(kind=8)::tend=0  ! end time for the simulation
    real(kind=8)::aend=0  ! end expansion factor
 
    ! Parameters to specify when to write an output
    namelist/output_params/noutput,foutput,aout,tout &
-   & ,tend,delta_tout,aend,delta_aout,gadget_output,walltime_hrs,minutes_dump &
-   & ,output_to_log,write_conservative,read_conservative,exact_output_time
+   & ,tend,delta_tout,aend,delta_aout,gadget_output,walltime_hrs,minutes_dump,output_to_log
 
    ! Go to the beginning of the file
    rewind(namelist_unit)
@@ -484,20 +483,55 @@ subroutine read_output_params(namelist_unit,nml_ok)
       nml_ok=.false.
    endif
 
+   ! Reset deprecated noutput namelist param.
+   ! The number of outputs is now counted automatically below.
+   if(noutput>0)then
+      if(myid==1)write(*,*)'Deprecated parameter "noutput" ignored.'
+   endif
+   noutput=0
+
    !-------------------------------------------------
-   ! Compute time step for outputs
+   ! Process output parameters
    !-------------------------------------------------
+
    ! check how many predetermined output times are listed (either give tout or aout)
-   if(noutput==0.and..not.all(tout==HUGE(1.0D0)))then
+   if(.not.all(tout==HUGE(1.0D0)))then
       do while(tout(noutput+1)<HUGE(1.0D0))
          noutput = noutput+1
       enddo
    endif
-   if(noutput==0.and..not.all(aout==HUGE(1.0D0)))then
+   if(.not.all(aout==HUGE(1.0D0)))then
       do while(aout(noutput+1)<HUGE(1.0D0))
          noutput = noutput+1
       enddo
    endif
+
+   ! check if the output time/expansion factor lists contain increasing values
+   do i=1,noutput-1
+      if(tout(i)>tout(i+1))then
+         if(myid==1)write(*,*)'Error: tout list not increasing.'
+         nml_ok=.false.
+         exit
+      endif
+      if(aout(i)>aout(i+1))then
+         if(myid==1)write(*,*)'Error: aout list not increasing.'
+         nml_ok=.false.
+         exit
+      endif
+   end do
+
+   ! check if tend/aend are consistent with tout/aout
+   if(noutput>0)then
+      if(tout(noutput)<HUGE(1.0D0).and.tend>0.and.tend<tout(noutput))then
+         if(myid==1)write(*,*)'Error: Inconsistent tend. Should be larger or equal to last tout.'
+         nml_ok=.false.
+      endif
+      if(aout(noutput)<HUGE(1.0D0).and.aend>0.and.aend<aout(noutput))then
+         if(myid==1)write(*,*)'Error: Inconsistent aend. Should be larger or equal to last aout.'
+         nml_ok=.false.
+      endif
+   endif
+
    ! add final time and expansion factor at the back of the predetermined output list
    if(tend>0)then
       noutput=noutput+1
@@ -507,6 +541,7 @@ subroutine read_output_params(namelist_unit,nml_ok)
       noutput=noutput+1
       aout(noutput)=aend
    endif
+
    ! set periodic output params
    tout_next=delta_tout
    aout_next=delta_aout

patch/init/dice/read_params.f90
@@ -62,7 +62,6 @@ subroutine read_params
   use mpi_mod
   use buildinfo
   use iso_fortran_env, ONLY: output_unit !standard output
-
   implicit none
   !--------------------------------------------------
   ! Local variables
@@ -470,7 +469,7 @@ subroutine read_amr_params(namelist_unit,nml_ok)
       if(myid==1)write(*,*)'You need to set up namelist &AMR_PARAMS in parameter file.'
       nml_ok=.false.
    elseif(nml_err>0)then
-      if(myid==1)write(*,*)'Error reading namelist &INIT_PARAMS. Check formatting.'
+      if(myid==1)write(*,*)'Error reading namelist &AMR_PARAMS. Check formatting.'
       nml_ok=.false.
    endif
 
@@ -524,7 +523,7 @@ subroutine read_output_params(namelist_unit,nml_ok)
    implicit none
    integer,intent(in)::namelist_unit
    logical,intent(inout)::nml_ok
-   integer::nml_err
+   integer::nml_err,i
    real(kind=8)::tend=0  ! end time for the simulation
    real(kind=8)::aend=0  ! end expansion factor
 
@@ -543,20 +542,55 @@ subroutine read_output_params(namelist_unit,nml_ok)
       nml_ok=.false.
    endif
 
+   ! Reset deprecated noutput namelist param.
+   ! The number of outputs is now counted automatically below.
+   if(noutput>0)then
+      if(myid==1)write(*,*)'Deprecated parameter "noutput" ignored.'
+   endif
+   noutput=0
+
    !-------------------------------------------------
-   ! Compute time step for outputs
+   ! Process output parameters
    !-------------------------------------------------
+
    ! check how many predetermined output times are listed (either give tout or aout)
-   if(noutput==0.and..not.all(tout==HUGE(1.0D0)))then
+   if(.not.all(tout==HUGE(1.0D0)))then
       do while(tout(noutput+1)<HUGE(1.0D0))
          noutput = noutput+1
       enddo
    endif
-   if(noutput==0.and..not.all(aout==HUGE(1.0D0)))then
+   if(.not.all(aout==HUGE(1.0D0)))then
       do while(aout(noutput+1)<HUGE(1.0D0))
          noutput = noutput+1
       enddo
    endif
+
+   ! check if the output time/expansion factor lists contain increasing values
+   do i=1,noutput-1
+      if(tout(i)>tout(i+1))then
+         if(myid==1)write(*,*)'Error: tout list not increasing.'
+         nml_ok=.false.
+         exit
+      endif
+      if(aout(i)>aout(i+1))then
+         if(myid==1)write(*,*)'Error: aout list not increasing.'
+         nml_ok=.false.
+         exit
+      endif
+   end do
+
+   ! check if tend/aend are consistent with tout/aout
+   if(noutput>0)then
+      if(tout(noutput)<HUGE(1.0D0).and.tend>0.and.tend<tout(noutput))then
+         if(myid==1)write(*,*)'Error: Inconsistent tend. Should be larger or equal to last tout.'
+         nml_ok=.false.
+      endif
+      if(aout(noutput)<HUGE(1.0D0).and.aend>0.and.aend<aout(noutput))then
+         if(myid==1)write(*,*)'Error: Inconsistent aend. Should be larger or equal to last aout.'
+         nml_ok=.false.
+      endif
+   endif
+
    ! add final time and expansion factor at the back of the predetermined output list
    if(tend>0)then
       noutput=noutput+1
@@ -566,6 +600,7 @@ subroutine read_output_params(namelist_unit,nml_ok)
       noutput=noutput+1
       aout(noutput)=aend
    endif
+
    ! set periodic output params
    tout_next=delta_tout
    aout_next=delta_aout
@@ -702,7 +737,8 @@ subroutine read_poisson_params(namelist_unit,nml_ok)
    integer::nml_err
 
    namelist/poisson_params/epsilon,gravity_type,gravity_params &
-   & ,cg_levelmin,cic_levelmax
+   & ,cg_levelmin,cic_levelmax,self_gravity,gravity_rho_ana_type,gravity_force_ana_type &
+   & ,gravity_rho_ana_params,gravity_force_ana_params
 
    ! Go to the beginning of the file
    rewind(namelist_unit)
@@ -712,6 +748,28 @@ subroutine read_poisson_params(namelist_unit,nml_ok)
 
    if(nml_err>0)then
       if(myid==1)write(*,*)'Error reading namelist &POISSON_PARAMS. Check formatting.'
+      nml_ok=.false.
+   endif
+
+   ! check for deprecated gravity_type parameter and apply old behaviour if needed
+   if(gravity_type.ne.0)then
+      if(myid==1)write(*,*)'Warning: gravity_type is now deprecated. Please use self_gravity, gravity_rho_ana_type and gravity_force_ana_type instead.'
+      if(gravity_type<0)then
+         self_gravity=.true.
+         gravity_rho_ana_type=-gravity_type
+         gravity_force_ana_type=0
+         gravity_rho_ana_params=gravity_params
+      else if(gravity_type>0)then
+         self_gravity=.false.
+         gravity_rho_ana_type=0
+         gravity_force_ana_type=gravity_type
+         gravity_force_ana_params=gravity_params
+      endif
+   endif
+
+   ! Currently you cannot have both, since then it is unclear what to do with the gravity_params input array
+   if(gravity_rho_ana_type>0.and.gravity_force_ana_type>0)then
+      if(myid==1)write(*,*)'Error: you cannot have both gravity_rho_ana_type and gravity_force_ana_type > 0 at the same time.'
       nml_ok=.false.
    endif
 

commit 11022ae9a96708263b75e5e224d226b9b2e7eb06

Fix output params (#306)
Fix a few things that were broken in PR #288 

Now that `noutput` is complemently ignored in the namelist, the
parameter `nstepmax` no longer worked as intended. When no output time
was given, tout(0) went out of bound, causing the simulation to stop
after 1 timestep. Setting noutput to a minimum of 1 keeps the target
output time to HUGE, as before, so that the stopping criterion becomes
nstepmax.

Restore` write_conservative,read_conservative,exact_output_time`
parameters to the namelist. They were accidentally deleted, making these
features inaccessible.

amr/read_params.f90
@@ -471,7 +471,8 @@ subroutine read_output_params(namelist_unit,nml_ok)
 
    ! Parameters to specify when to write an output
    namelist/output_params/noutput,foutput,aout,tout &
-   & ,tend,delta_tout,aend,delta_aout,gadget_output,walltime_hrs,minutes_dump,output_to_log
+   & ,tend,delta_tout,aend,delta_aout,gadget_output,walltime_hrs,minutes_dump &
+   & ,output_to_log,write_conservative,read_conservative,exact_output_time
 
    ! Go to the beginning of the file
    rewind(namelist_unit)
@@ -542,6 +543,9 @@ subroutine read_output_params(namelist_unit,nml_ok)
       noutput=noutput+1
       aout(noutput)=aend
    endif
+
+   ! if no output time was requested at all, keep a single HUGE entry in the list
+   if(noutput==0)noutput=1
 
    ! set periodic output params
    tout_next=delta_tout